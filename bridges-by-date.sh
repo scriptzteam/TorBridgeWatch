121e401a437512b20c3ed8cd47b05b30d3790802
[sCRiPTz-TEAM] <scriptzteam@users.noreply.github.com>
2025-07-30 15:19:36 +0200

    Create bridges-by-date.sh

---
+++ bridges-by-date.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkdir -p "$(date +%Y)/$(date +%m)/$(date +%d)"
+
+echo '| Nickname |  Contact | Hashed Fingerprint	| Running | Flags | Last Seen | First Seen | Last Restarted | Advertised Bandwidth | Platform | Version | Version Status | Recommended Version | BridgeDB Distributor | OR Addresses | Transports | BlockList |' > "$(date +%Y)/$(date +%m)/$(date +%d)"/Readme.md
+echo '|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|' >> "$(date +%Y)/$(date +%m)/$(date +%d)"/Readme.md
+
+TODAY=$(date +%F) # gets YYYY-MM-DD
+
+curl -s "https://onionoo.torproject.org/details" | jq -r --arg TODAY "$TODAY" '
+  .bridges[]
+  | select((.first_seen // "") | startswith($TODAY))
+  | [
+      (.nickname // "N/A"),
+      (.contact // "N/A"),
+      (.hashed_fingerprint // "N/A"),
+      (.running | tostring),
+      ((.flags // []) | join(", ")),
+      (.last_seen // "N/A"),
+      (.first_seen // "N/A"),
+      (.last_restarted // "N/A"),
+      ((.advertised_bandwidth // 0) | tostring),
+      (.platform // "N/A"),
+      (.version // "N/A"),
+      (.version_status // "N/A"),
+      ((.recommended_version // false) | tostring),
+      (.bridgedb_distributor // "N/A"),
+      ((.or_addresses // []) | join(", ")),
+      ((.transports // []) | join(", ")),
+      ((.blocklist // []) | join(", "))
+    ]
+  | map(tostring | gsub("\\|"; "\\|") | gsub("\\\\"; "\\\\"))
+  | join(" | ")
+  | "|" + . + "|"
+' >> "$(date +%Y)/$(date +%m)/$(date +%d)"/Readme.md
